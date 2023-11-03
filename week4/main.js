9376c9bf68a9dac4663e1fb2de16670e2e5cb13e
asmasathar7 <asp2002@hw.ac.uk>
2023-11-03 10:05:07 +0400

    new commit

---
+++ week4/main.js
@@ -0,0 +1,24 @@
+var myCity = [
+    {
+        "name": "Dubai",
+        "country": "UAE",
+        "place": "Dubai Mall"
+        } ,
+    {
+        "name": "Dubai",
+        "country": "italy",
+        "place": "Domo di milan "
+        } ,
+    {
+        "name": "Dubai",
+        "country": "UAE",
+        "place": "Marina Mall"
+        },   
+]
+  
+console.log(myCity[1].country);
+console.log(myCity.name);  
+
+var myCity2=["Abu Dhabi","UAE"];
+
+console.log(myCity2[1])
