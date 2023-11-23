d762bd3d4955607d471a0965bc8456b3a6e9293e
asmasathar7 <asp2002@hw.ac.uk>
2023-11-24 01:12:18 +0400

    new commit

---
+++ week5/index.js
@@ -0,0 +1,48 @@
+
+const express=require('express');
+const mysql=require('mysql');
+const dotenv=require('dotenv');
+dotenv.config({path:'./.env'});
+const cookieParser=require('cookie-parser');
+const path=require('path');
+const app=express();
+const port=5000;
+
+const db=mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user:process.env.DATABASE_USER,
+    password:process.env.DATABASE_PASSWORD,
+    database:process.env.DATABASE,
+    port:3306
+});
+
+
+
+
+const publicDirectory=path.join(__dirname,'./public');
+app.use(express.static(publicDirectory));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use(cookieParser());
+app.set('view engine','hbs');
+
+
+db.connect((error)=>{
+    if(error){
+        console.log(error)
+    }
+    else{
+        console.log("mySql connected")
+    }
+});
+
+module.exports=db;
+
+app.use('/',require('./routes/pages'));
+app.use('/auth',require('./routes/auth'));
+
+
+
+app.listen(port, () =>{
+    console.log(`Server started on port ${port}. `);
+});
---
+++ week5/routes/pages.js
@@ -0,0 +1,31 @@
+const express=require('express');
+const router=express.Router();
+authController=require('../controllers/auth');
+
+
+router.get('/',(req,res) => {
+    res.render("index");
+});
+
+router.get('/register',(req,res) => {
+    res.render('register');
+});
+
+router.get('/login',(req,res)=>{
+    res.render('login');
+});
+
+
+router.get('/profile', (req, res) => {
+    const user = {
+        id: req.query.id,
+        name: req.query.name,
+        email: req.query.email
+    };
+   
+    res.render('profile', { user });
+});
+
+
+module.exports=router;
+   
